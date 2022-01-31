ea63cf537be6dd6e678cb6eb6665d60339309d95
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-02-01 00:04:42 +0100

    Nearby restaurant completed

--- lib/widgets/rating_stars.dart
+++ lib/widgets/rating_stars.dart
@@ -8,7 +8,7 @@ class RatingStars extends StatelessWidget {
   Widget build(BuildContext context) {
     String stars = '';
     for (int i = 0; i < rating; i++) {
-      stars += '⭐ ';
+      stars += '⭐  ';
     }
     stars.trim();
     return Text(
