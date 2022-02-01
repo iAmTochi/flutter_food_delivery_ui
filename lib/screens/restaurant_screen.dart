7f9d10fda605e52576bb69f4d9cb1daa12529fa9
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-02-01 15:11:49 +0100

    stack and hero animation

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -84,6 +84,37 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
               ],
             ),
           ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Reviews',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  )),
+              TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.symmetric(horizontal: 30.0),
+                    backgroundColor: Theme.of(context).primaryColor,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                  ),
+                  child: Text(
+                    'Contact',
+                    style: TextStyle(color: Colors.white, fontSize: 20.0),
+                  ))
+            ],
+          ),
         ],
       ),
     );
