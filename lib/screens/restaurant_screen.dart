672677ae483b45f8d00a5e163c4c842d08297b7f
Tochukwu Ugwu <ugwukelvintochukwu@gmail.com>
2022-02-01 15:11:22 +0100

    added background

--- lib/screens/restaurant_screen.dart
+++ lib/screens/restaurant_screen.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_food_delivery_ui/models/restaurant.dart';
+import 'package:flutter_food_delivery_ui/widgets/rating_stars.dart';
 
 class RestaurantScreen extends StatefulWidget {
-
   final Restaurant restaurant;
-   RestaurantScreen({this.restaurant});
+  RestaurantScreen({this.restaurant});
 
   @override
   _RestaurantScreenState createState() => _RestaurantScreenState();
@@ -14,14 +14,77 @@ class _RestaurantScreenState extends State<RestaurantScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: Icon(Icons.account_circle),
-          iconSize: 30.0,
-          onPressed: () {},
-        ),
-        title: Text('Restaurants  Nearby'),
-
+      body: Column(
+        children: [
+          Stack(
+            children: [
+              Hero(
+                tag: widget.restaurant.imageUrl,
+                child: Image(
+                  height: 220.0,
+                  width: MediaQuery.of(context).size.width,
+                  image: AssetImage(widget.restaurant.imageUrl),
+                  fit: BoxFit.cover,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: Icon(Icons.arrow_back_ios),
+                      color: Colors.white,
+                      iconSize: 30.0,
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite),
+                      color: Theme.of(context).primaryColor,
+                      iconSize: 35.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      widget.restaurant.name,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      'O.2 miles away',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                  ],
+                ),
+                RatingStars(widget.restaurant.rating),
+                SizedBox(height: 6.0),
+                Text(
+                  widget.restaurant.address,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
